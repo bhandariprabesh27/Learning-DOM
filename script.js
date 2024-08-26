d639129e0d01a758f46f6a7b079d7cf90aaab2df
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-08-26 22:41:23 +0545

    Accessing Elements

---
+++ script.js
@@ -0,0 +1,6 @@
+var frstimg = document.querySelector("#abcd");
+var secndimg = document.querySelector(".eddf");
+var firsth3 = document.querySelector("#abcd");
+var secndh3 = document.querySelector(".abcd");
+
+//Eg: Manam hami snga html ma dherai h3 xa ra JS ma document.querySelector("h3") garda first ko matra select hunxa. Yo case ma sabai h3 lai select garna ko lagi document.querySelectorAll("h3") garnu parxa.
